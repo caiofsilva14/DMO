70feb32d1f423765ce8f876a5ca09cb5a0de76f7
caiofsilva14 <caiofsilva14@gmail.com>
2024-02-16 22:35:16 -0400

    Adiciona a pasta do servidor e arquivos para funcionamento

---
+++ Laragon/phpMyAdmin/tmp/twig/e7/e7c5b489ad64df7db3eca69142182c9fc1a987042ddcc71a67be5ff30bcd28d5.php
@@ -0,0 +1,67 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* display/results/value_display.twig */
+class __TwigTemplate_ef8a84426988b338383e57916793b8d8cd0fa592c81f785d84cb80650565b6b0 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<td class=\"left ";
+        echo twig_escape_filter($this->env, ($context["class"] ?? null), "html", null, true);
+        echo ((($context["condition_field"] ?? null)) ? (" condition") : (""));
+        echo "\">";
+        // line 2
+        echo ($context["value"] ?? null);
+        // line 3
+        echo "</td>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "display/results/value_display.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  44 => 3,  42 => 2,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "display/results/value_display.twig", "C:\\laragon\\etc\\apps\\phpMyAdmin\\templates\\display\\results\\value_display.twig");
+    }
+}
